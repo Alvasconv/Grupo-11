a15e556693b95960e86e0e6e4b56ff1412b85243
ljbarzol <ljbarzol@espol.edu.ec>
2024-06-03 01:41:49 -0500

    Creacion pantalla principal y pantalla añadirVehiculo

--- Grupo_11/src/main/java/com/grupo_11/PrimaryController.java
+++ Grupo_11/src/main/java/com/grupo_11/PrimaryController.java
@@ -1,12 +1,35 @@
 package com.grupo_11;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 
 public class PrimaryController {
 
     @FXML
+    private Button botonAñadir;
+    @FXML
+    private Button botonEditar;
+    @FXML
+    private Button botonRemover;
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    private void añadirVehiculo(ActionEvent event) throws IOException {
+        App.setRoot("añadirV");
+    }
+
+    @FXML
+    private void editarVehiculo(ActionEvent event) {
+    }
+
+    @FXML
+    private void RemoverVehiculo(ActionEvent event) {
+    }
+    
+
 }
--- Grupo_11/src/main/java/module-info.java
+++ Grupo_11/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.grupo_11 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.grupo_11 to javafx.fxml;
     exports com.grupo_11;
